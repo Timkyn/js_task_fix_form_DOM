8a1d998f7bb1f6ae98351b735a6b7e540cc3f85a
Tymofii Fastovetskyi <timoxa.sykunov2017@gmail.com>
2024-12-15 17:24:36 +0200

    get all inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
+// get all 'inputs' from 'form' tag on the page
+const forms = document.body.querySelectorAll('form');
+const inputs = [];
 
-// write code here
+for (let f = 0; f < forms.length; f++) {
+  const form = forms[f].querySelectorAll('input');
+
+  for (let i = 0; i < form.length; i++) {
+    inputs.push(form[i]);
+  }
+}
+
+console.log(inputs);
