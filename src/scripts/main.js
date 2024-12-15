31f93bf70a1cc0352f61ae4a782f1ab760258194
Tymofii Fastovetskyi <timoxa.sykunov2017@gmail.com>
2024-12-15 21:26:18 +0200

    get task donne

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,4 +11,24 @@ for (let f = 0; f < forms.length; f++) {
   }
 }
 
-console.log(inputs);
+// set placeholder for each input
+for (const input of inputs) {
+  const text = input.getAttribute('name');
+  const capitalized = text.charAt(0).toUpperCase() + text.slice(1);
+  input.setAttribute('placeholder', capitalized);
+}
+
+// step 2 + 4 , create and append label to input
+for (const input of inputs) {
+  // create label
+  const label = document.createElement('label');
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.getAttribute('id'));
+  label.textContent = input.getAttribute('name');
+
+  // append label
+
+  input.parentElement.appendChild(label);
+}
+
+
